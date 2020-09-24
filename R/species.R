5640837fa7d8334bc1cbb6338f9039b89574a9d9
pierredenelle <pierre.denelle@gmail.com>
2020-09-24 15:56:40 +0100

    species.R and .Rd created

---
+++ R/species.R
@@ -0,0 +1,14 @@
+
+species <- function(){
+  require(jsonlite)
+  require(dplyr)
+  
+  # Return the species names
+  tmp <- read_json(paste0(
+    "http://",credentials[[1]], ":",credentials[[2]],
+    "@gift.uni-goettingen.de/api/extended/index.php?query=species"),
+    simplifyVector = TRUE)
+  
+  return(tmp)
+}
+
