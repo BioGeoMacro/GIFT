62c320a2518be5ce42f57976d81aa5592fc4d263
Patrick Weigelt <pweigel@uni-goettingen.de>
2020-09-24 13:40:46 +0200

    setup folders and files

---
+++ R/get_credentials.R
@@ -0,0 +1,6 @@
+get_credentials = function(){
+  username <- readline("Enter username: ")
+  password <- readline("Enter password: ")
+  assign(x = "credentials", value = list(username,password), envir = globalenv())
+  lockBinding("credentials", globalenv())
+}
