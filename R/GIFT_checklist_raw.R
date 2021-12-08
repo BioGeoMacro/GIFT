dadf35114bc385094f9940bea04fc089501e3285
denelle <pierre.denelle@gmail.com>
2021-12-08 10:11:23 +0100

    GIFT_checklist_raw updated

--- R/GIFT_checklist_raw.R
+++ R/GIFT_checklist_raw.R
@@ -1,7 +1,7 @@
 
 GIFT_checklist_raw <- function(
-  list_ID, taxonid = 1, include_names_unique = FALSE,
-  api = "http://gift.uni-goettingen.de/api/extended/",
+  list_ID, taxonid = 1, namesmatched = TRUE,
+  api = "http://gift.uni-goettingen.de/api/extended/"
   # potential arguments not implemented yet
   # endemic_ref = 0, endemic_list = 0, native = 0, naturalized = 0,
   # ref_ID
@@ -13,20 +13,31 @@ GIFT_checklist_raw <- function(
   require(jsonlite)
   
   # Arguments
-  if(!is.logical(include_names_unique)){
-    stop("'include_names_unique' must be a boolean indicating whether you want access to original name.")
+  if(length(list_ID) == 0){
+    stop("Please provide the ID numbers of the checklists you want to load.")
+  }
+  
+  if(!is.numeric(taxonid)){
+    stop("'taxonid' is a numeric describing what taxonomic group you want.
+         See help of the function.")
+  }
+  
+  if(!is.logical(namesmatched)){
+    stop("'namesmatched' must be a boolean indicating whether you want access to original name.")
   }
   
   # 2. Query ----
-  tmp <- read_json(paste0(
-    "https://", credentials[[1]], ":",credentials[[2]],
-    "@gift.uni-goettingen.de/api/extended/index.php?query=checklists&listid=",
-    as.numeric(list_ID), "&taxonid=", as.numeric(taxonid),
-    "&include_names_unique=", include_names_unique),
-    simplifyVector = TRUE)
-  
-  return(tmp)
-  
-  # https://gift.uni-goettingen.de/api/extended/?query=checklists&listid=100&taxonid=5&namesmatched=1
-  
+  list_raw <- c()
+  for(i in seq_along(list_ID)){
+    tmp <- read_json(paste0(
+      "https://", credentials[[1]], ":",credentials[[2]],
+      "@gift.uni-goettingen.de/api/extended/index.php?query=checklists&listid=",
+      as.numeric(list_ID[i]), "&taxonid=", as.numeric(taxonid),
+      "&namesmatched=", as.numeric(namesmatched)),
+      simplifyVector = TRUE)
+    
+    list_raw <- bind_rows(list_raw, tmp)
+  }
+
+  return(list_raw)
 }
