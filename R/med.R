f2fccb8ee94d29099ca572335d747de2285cc2b0
denelle <pierre.denelle@gmail.com>
2021-12-10 09:26:26 +0100

    R and man for Mediterranean shapefile

---
+++ R/med.R
@@ -0,0 +1,17 @@
+#'
+#' Shapefile of Mediterreanean region
+#'
+#' Shapefile encompassing the Mediterreanean sea. CRS is WGS84.
+#'
+#' @docType data
+#'
+#' @usage data("med")
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(med)
+"med"
