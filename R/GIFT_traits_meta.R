6612d50bc8030efe07d6011d21baeef77f9f9c32
denelle <pierre.denelle@gmail.com>
2022-03-17 14:47:58 +0100

    GIFT_traits_meta() updated

--- R/GIFT_traits_meta.R
+++ R/GIFT_traits_meta.R
@@ -1,22 +1,37 @@
+#' GIFT_traits_meta
+#'
+#' Retrieve metadata of the functional traits.
+#'
+#' @param api Character string corresponding to the API.
+#' 
+#' @return
+#' data frame with metadata about traits available in GIFT.
+#'
+#' @details Blabla.
+#'
+#' @references
+#'      Weigelt, P, König, C, Kreft, H. GIFT – A Global Inventory of Floras and
+#'      Traits for macroecology and biogeography. J Biogeogr. 2020; 47: 16– 43.
+#'      https://doi.org/10.1111/jbi.13623
+#'
+#' @seealso [GIFT::GIFT_traits()]
+#'
+#' @examples
+#' \dontrun{
+#' ex <- GIFT_traits_meta()
+#' }
+#' 
+#' @importFrom jsonlite read_json
+#' 
+#' @export
 
-GIFT_traits_meta <- function(restricted = FALSE){
+GIFT_traits_meta <- function(
+  api = "http://gift.uni-goettingen.de/api/extended/index.php"){
   # 1. Controls ----
-  # Package dependencies
-  require(jsonlite)
-  require(dplyr)
 
-  if(!is.logical(restricted)){
-    stop("'restricted' must be a boolean indicating whether you want access to
-         restricted data.")
-  }
-  
   # 2. Query ----
-  tmp <- read_json(paste0(
-    "http://", credentials[[1]], ":",credentials[[2]],
-    "@gift.uni-goettingen.de/api/extended/index.php?query=traits_meta&restricted=",
-    as.numeric(restricted)),
-    simplifyVector = TRUE)
-  
+  tmp <- jsonlite::read_json(paste0(api, "?query=traits_meta"),
+                             simplifyVector = TRUE)
+
   return(tmp)
 }
-
