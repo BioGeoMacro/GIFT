e289f337a62a04f7d71fb7e6b3a0641057784e9b
Patrick Weigelt <pweigel@uni-goettingen.de>
2022-08-19 11:09:27 +0200

    api and version GIFT_species()

--- R/GIFT_species.R
+++ R/GIFT_species.R
@@ -1,12 +1,13 @@
 
-GIFT_species <- function(api = "http://gift.uni-goettingen.de/api/extended/index.php"){
+GIFT_species <- function(api = "http://gift.uni-goettingen.de/api/extended/",
+                         GIFT_version = NULL){
   require(jsonlite)
   require(dplyr)
   
   # Return the species names
   tmp <- read_json(paste0(
-    api,
-    "?query=species"),
+    api, "index", ifelse(is.null(GIFT_version), "", GIFT_version),
+    ".php?query=species"),
     simplifyVector = TRUE)
   
   return(tmp)
