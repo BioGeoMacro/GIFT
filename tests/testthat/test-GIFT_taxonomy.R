f214261a6861fd37b5caf6190b640916c2c9efa5
denelle <pierre.denelle@gmail.com>
2022-09-22 11:53:49 +0200

    Test updated for GIFT_taxonomy

--- tests/testthat/test-GIFT_taxonomy.R
+++ tests/testthat/test-GIFT_taxonomy.R
@@ -8,3 +8,10 @@ test_that("data frame output format", {
   expect_identical(ncol(ex), c(6L))
   
 })
+
+# Tests for invalid inputs ----
+
+# Error message when ref_ID and list_ID are missing
+expect_error(
+  GIFT_taxonomy(api = 1),
+  "api must be a character string indicating which API to use.", fixed = TRUE)
