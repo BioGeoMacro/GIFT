d1751c0c48f18f8e421b985c00553ca78a3fe95d
denelle <pierre.denelle@gmail.com>
2022-09-27 22:24:01 +0200

    tests for GIFT_env_meta_misc() and GIFT_env_meta_raster()

---
+++ tests/testthat/test-GIFT_env_meta_raster.R
@@ -0,0 +1,23 @@
+# Tests for valid outputs ----
+
+# Output should be a dataframe with 5 columns
+test_that("data frame output format", {
+  ex <- GIFT_env_meta_raster()
+  
+  expect_s3_class(ex, "data.frame")
+  expect_identical(ncol(ex), c(10L))
+  
+})
+
+# Tests for invalid inputs ----
+
+# Error message when ref_ID and list_ID are missing
+expect_error(
+  GIFT_env_meta_raster(api = NA),
+  "api must be a character string indicating which API to use.", fixed = TRUE)
+
+expect_error(
+  GIFT_env_meta_raster(GIFT_version = NA),
+  "'GIFT_version' must be a character string stating what version
+    of GIFT you want to use. Available options are 'latest' and the different
+           versions.", fixed = TRUE)
