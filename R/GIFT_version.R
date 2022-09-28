1457744364c7a129eff3e41376f9e95ba3e74a6a
denelle <pierre.denelle@gmail.com>
2022-09-28 10:54:25 +0200

    GIFT_version() created with header and test

---
+++ R/GIFT_version.R
@@ -0,0 +1,35 @@
+#' Version of GIFT available
+#'
+#' Returns a table with information on the different versions of the database
+#'
+#' @return
+#' A data frame with 4 columns.
+#'
+#' @details Here is what each column refers to:
+#' 'ID' - Identification number of the version.
+#' 'version'- Version number.
+#' 'description'- What were the major updates about.
+#'
+#' @references
+#'      Weigelt, P, König, C, Kreft, H. GIFT – A Global Inventory of Floras and
+#'      Traits for macroecology and biogeography. J Biogeogr. 2020; 47: 16– 43.
+#'      https://doi.org/10.1111/jbi.13623
+#'
+#' @seealso [GIFT::GIFT_checklist()]
+#'
+#' @examples
+#' \dontrun{
+#' ex <- GIFT_version()
+#' }
+#' 
+#' @importFrom jsonlite read_json
+#' 
+#' @export
+
+GIFT_version <- function(){
+  gift_version <- jsonlite::read_json(
+    "https://gift.uni-goettingen.de/api/index.php?query=versions",
+    simplifyVector = TRUE)
+  
+  return(gift_version)
+}
---
+++ tests/testthat/test-GIFT_version.R
@@ -0,0 +1,10 @@
+# Tests for valid outputs ----
+
+# Output should be a dataframe with 5 columns
+test_that("data frame output format", {
+  ex <- GIFT_version()
+  
+  expect_s3_class(ex, "data.frame")
+  expect_identical(ncol(ex), c(4L))
+  
+})
