f79f3b130cb1104ae9fc567b79183f88e033ec97
denelle <pierre.denelle@gmail.com>
2022-10-12 16:42:27 +0200

    get_credentials deleted

--- R/get_credentials.R
+++
@@ -1,7 +0,0 @@
-get_credentials <- function(){
-  username <- readline("Enter username: ")
-  password <- readline("Enter password: ")
-  assign(x = "credentials", value = list(username, password),
-         envir = globalenv())
-  lockBinding("credentials", globalenv())
-}
