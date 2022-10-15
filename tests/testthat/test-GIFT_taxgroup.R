bf089b0515026b88e9f30aaf21564e18f9338c88
denelle <pierre.denelle@gmail.com>
2022-10-15 22:52:00 +0200

    GIFT_taxgroup() test

---
+++ tests/testthat/test-GIFT_taxgroup.R
@@ -0,0 +1,28 @@
+# Tests for valid outputs ----
+
+# Output should be a dataframe with 5 columns
+test_that("data frame output format", {
+  ex <- GIFT_taxgroup(work_ID = c(1:5), taxon_lvl = "family")
+  
+  expect_identical(class(ex), "character")
+  expect_identical(length(ex), c(5L))
+  
+})
+
+# Tests for invalid inputs ----
+
+# Error message when ref_ID and list_ID are missing
+expect_error(
+  GIFT_taxgroup(api = NA),
+  "api must be a character string indicating which API to use.", fixed = TRUE)
+
+expect_error(
+  GIFT_taxgroup(GIFT_version = NA),
+  "'GIFT_version' must be a character string stating what version
+    of GIFT you want to use. Available options are 'latest' and the different
+           versions.", fixed = TRUE)
+
+expect_message(GIFT_taxgroup(GIFT_version = "beta"),
+               "You are asking for the beta-version of GIFT which is subject to
+            updates and edits. Consider using 'latest' for the latest stable
+            version.")
