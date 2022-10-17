9e9648780003c4dff4b7177a5191bcc8fee91f22
denelle <pierre.denelle@gmail.com>
2022-10-17 10:23:10 +0200

    More tests for GIFT_spatial()

--- tests/testthat/test-GIFT_spatial.R
+++ tests/testthat/test-GIFT_spatial.R
@@ -1,9 +1,10 @@
 # Tests for valid outputs ----
+# Mediterranean shape
+data("med")
 
 # Output should be a list with two data frames
 test_that("data frame output format",
           {
-            data("med")
             ex <-
               GIFT_spatial(shp = med, overlap = "centroid_inside")
             
@@ -13,7 +14,37 @@ test_that("data frame output format",
           })
 
 # Tests for invalid inputs ----
-
-expect_error(
-  GIFT_spatial(shp = NULL, coordinates = NULL),
-  "Please provide a shapefile or a set of XY coordinates.", fixed = TRUE)
+test_that("data frame output format",
+          {
+            expect_error(
+              GIFT_spatial(shp = NULL, coordinates = NULL),
+              "Please provide a shapefile or a set of XY coordinates.",
+              fixed = TRUE)
+            
+            expect_error(
+              GIFT_spatial(shp = med, overlap = "error"),
+              "overlap is a character string indicating whether you want to use
+         centroid or extent of GIFT polygons to overlap with your shapefile.\n
+         It has to be 'centroid_inside', 'shape_inside', 'shape_intersect' or
+         'extent_intersect'.",
+              fixed = TRUE)
+            
+            expect_error(
+              GIFT_spatial(shp = med, api = NA),
+              "api must be a character string indicating which API to use.",
+              fixed = TRUE)
+            
+            expect_error(
+              GIFT_spatial(shp = med, GIFT_version = NA),
+              "'GIFT_version' must be a character string stating what version
+    of GIFT you want to use. Available options are 'latest' and the different
+           versions.",
+              fixed = TRUE)
+            
+            expect_message(
+              GIFT_spatial(GIFT_version = "beta"),
+              "You are asking for the beta-version of GIFT which is subject to
+            updates and edits. Consider using 'latest' for the latest stable
+            version.")
+            
+          })
