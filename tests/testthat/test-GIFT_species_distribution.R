e9fb1ba51c835be1a93f36f3b88196597eee808d
denelle <pierre.denelle@gmail.com>
2022-10-20 12:03:47 +0200

    GIFT_species_distribution() First test

---
+++ tests/testthat/test-GIFT_species_distribution.R
@@ -0,0 +1,23 @@
+# Tests for valid outputs ----
+
+# Output should be a dataframe with 5 columns
+test_that("data frame output format", {
+  ex <- GIFT_species_distribution()
+  
+  expect_s3_class(ex, "data.frame")
+  expect_identical(ncol(ex), c(33L))
+  
+})
+
+# Tests for invalid inputs ----
+
+# Error message when ref_ID and list_ID are missing
+expect_error(
+  GIFT_species_distribution(api = NA),
+  "api must be a character string indicating which API to use.", fixed = TRUE)
+
+expect_error(
+  GIFT_species(GIFT_species_distribution = NA),
+  "'GIFT_version' must be a character string stating what version
+    of GIFT you want to use. Available options are 'latest' and the different
+           versions.", fixed = TRUE)
