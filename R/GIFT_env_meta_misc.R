e9b78e5bee0ae4274b18f18188881991ee4631fd
unknown <pierre.denelle@gmail.com>
2022-12-14 12:26:26 +0100

    Controls in a not exported function (start and try)

--- R/GIFT_env_meta_misc.R
+++ R/GIFT_env_meta_misc.R
@@ -43,32 +43,9 @@ GIFT_env_meta_misc <- function(
     api = "https://gift.uni-goettingen.de/api/extended/",
     GIFT_version = "latest"){
   # 1. Controls ----
-  # Arguments
-  if(length(api) != 1 || !is.character(api)){
-    stop("api must be a character string indicating which API to use.")
-  }
-  
-  # GIFT_version
-  gift_version <- jsonlite::read_json(
-    "https://gift.uni-goettingen.de/api/index.php?query=versions",
-    simplifyVector = TRUE)
-  if(length(GIFT_version) != 1 || is.na(GIFT_version) ||
-     !is.character(GIFT_version) || 
-     !(GIFT_version %in% c(unique(gift_version$version),
-                           "latest", "beta"))){
-    stop(c("'GIFT_version' must be a character string stating what version
-    of GIFT you want to use. Available options are 'latest' and the different
-           versions."))
-  }
-  if(GIFT_version == "latest"){
-    GIFT_version <- gift_version[nrow(gift_version), "version"]
-  }
-  if(GIFT_version == "beta"){
-    message("You are asking for the beta-version of GIFT which is subject to
-            updates and edits. Consider using 'latest' for the latest stable
-            version.")
-  }
-  
+  check_api(api)
+  check_gift_version(GIFT_version)
+
   # 2. Function ----
   # Return the miscellaneous environmental information as a data frame
   tmp <- jsonlite::read_json(paste0(
---
+++ R/check_functions.R
@@ -0,0 +1,54 @@
+# Internal functions to check inputs and outputs errors
+#
+# Authors: Pierre Denelle & Patrick Weigelt
+#
+
+# Checking api argument
+#
+# Authors: Pierre Denelle
+#
+# Stop if GIFT_version argument is not a proper version of the database
+#
+# Args:
+#   api a character
+#
+# Returns:
+#   nothing, shows an error message
+
+check_api <- function(api) {
+  if(length(api) != 1 || !is.character(api)){
+    stop("api must be a character string indicating which API to use.")
+  }
+}
+
+# Checking GIFT_version argument
+#
+# Authors: Pierre Denelle & Matthias Grenié
+#
+# Stop if GIFT_version argument is not a proper version of the database
+#
+# Args:
+#   GIFT_version a character
+#
+# Returns:
+#   nothing, shows an error message
+
+check_gift_version <- function(GIFT_version) {
+  if (length(GIFT_version) != 1 || is.na(GIFT_version) ||
+      !is.character(GIFT_version)) {
+    stop(c("'GIFT_version' must be a character string stating what version
+of GIFT you want to use. Available options are 'latest' and the different
+versions."))
+  }
+  if (GIFT_version == "latest") {
+    gift_version <- jsonlite::read_json(
+      "https://gift.uni-goettingen.de/api/index.php?query=versions",
+      simplifyVector = TRUE)
+    GIFT_version <- gift_version[nrow(gift_version), "version"]
+  }
+  if (GIFT_version == "beta") {
+    message("You are asking for the beta-version of GIFT which is subject to
+updates and edits. Consider using 'latest' for the latest stable
+version.")
+  }
+}
